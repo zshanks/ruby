f30b78561eb46f0df97e6ef9b2e2bbd74e6ffe1a
zshanks <zachthegreat1@gmail.com>
2013-09-25 16:53:29 +0000

    trying to pull update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
   def create
     #@user = User.new(:username => params[:username],:password => params[:password])
     #render 'new'
-		@user = User.new(params[:user])
-		@user.save
+    
+		permitted_params = params.require(:user).permit(:username, :password, :password_confirmation)
+		@user = User.new(permitted_params)
+		if @user.save then
+			redirect_to @user #type of response that the server can give to the browser.
+		else
+			render 'new'
+		end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
 	# attr_reader :username, :password, :confirmation
 	# attr_writer :username, :password, :confirmation
 	has_secure_password
+	
+	validates :username, presence: true
 end
