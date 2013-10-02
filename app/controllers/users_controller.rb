b80f86ffd326921c940611eb2b893b2c4ac2510c
zshanks <zachthegreat1@gmail.com>
2013-10-02 02:22:46 +0000

    getting updated information done in class

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,16 @@ class UsersController < ApplicationController
 			render 'new'
 		end
   end
+	
+	def index
+		@users = User.all
+	end
+
+	def show
+		@user - User.find(params[:id])
+	end
+	
+	def edit
+		@user - User.find(params[:id])
+	end	
 end
