5371660ed4db674d27914940bbab9026f8ea2e99
zshanks <zachthegreat1@gmail.com>
2013-12-08 00:21:07 +0000

    completing test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
-  before_action :ensure_user_logged_in, only: [:edit, :update]
+  #before_action :ensure_user_logged_in, only: [:edit, :update]
+  
+  def new
+  end
 	
 	def create
 		@user = User.find_by(username: params[:username])
@@ -14,44 +17,8 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
+    cookies.delete(:user_id)
+    flash[:info] = "You have successfully logged out"
+    redirect_to root_path
 	end
-  
-  def new
-		@user = User.new
-	end
-    
-  def show
-    @user = User.find(params[:id])
-  end
-		
-  def index
-    @users = User.all
-  end
-    
-  def edit
-    @user = User.find(params[:id])
-  end
-    
-  def update
-    @user = User.find(params[:id])
-    if @user.update(permitted_params) then
-			redirect_to @user
-		else
-      render 'edit'
-		end
-  end
-    
-  def destroy
-		@user = User.find(params[:id]).destroy
-    redirect_to users_path
-  end  
-    
-  private 
-    def permitted_params
-      params.require(:user).permit(:username, :password, :password_confirmation, :email)
-    end
-  
-    def ensure_user_logged_in
-      redirect_to root_path unless logged_in?
-    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :ensure_user_logged_in, only: [:edit, :update]
-  before_action :ensure_correct_user, only: [:edit, :update]
+	before_action :ensure_user_logged_in, only: [:edit,:update,:index]
+  before_action :ensure_correct_user, only: [:edit,:update]
   before_action :ensure_admin_user, only: [:destroy]
-  before_action :ensure_admin_user, only: [:edit, :update]
+	before_action :ensure_not_logged_in, only: [:new,:create]
   
   respond_to :html, :json, :xml
   
@@ -15,12 +15,9 @@ class UsersController < ApplicationController
 		@user = User.new(permitted_params)
     if @user.save then
 			flash[:success] = "Welcome to the site: #{@user.username}"
-      login @user
-			redirect_to @user #type of response that the server can give to the browser.
-		else
-			render 'new'
+			cookies.signed[:user_id] = @user.id
 		end
-    respond_with(@user)
+			redirect_to @user #type of response that the server can give to the browser.
   end
     
   def show
@@ -32,37 +29,47 @@ class UsersController < ApplicationController
     @users = User.all
     respond_with(@users)
   end
+		
+	def edit
+	end
     
   def update
+		@user = User.find(params[:id])
     if @user.update(permitted_params) then
-			redirect_to @user
-		else
-      render 'edit'
+			flash[:success] = "Successfully made updates to #{@username}"
 		end
     respond_with(@user)
   end
     
   def destroy
-		@user = User.find(params[:id]).destroy
-    redirect_to users_path
-    respond_with(@user)
-  end  
-    
+		@user = User.find(params[:id])
+		if @user.admin
+			redirect_to root_path
+		end
+		@user.destroy
+		flash[:success] = "Successfully deleted user"
+		respond_with(@user)
+  end
+	
+  def ensure_user_logged_in
+      redirect_to login_path unless logged_in?
+	end
+		
+	def ensure_correct_user
+		@user = User.find(params[:id])
+		redirect_to root_path, flash: {:danger => "Can't perform that action!"} unless current_user?(@user)
+	end
+		
+	def ensure_admin_user
+		redirect_to root_path, flash: {:danger => "Can't perform that action!"} unless current_user.admin?
+	end
+	
+	def ensure_not_logged_in
+		redirect_to root_path, flash: {:danger => "Can't perform that action!"} unless !logged_in?
+	end
+		
   private 
     def permitted_params
       params.require(:user).permit(:username, :password, :password_confirmation, :email)
-    end
-    
-    def ensure_user_logged_in
-      redirect_to root_path unless logged_in?
-    end
-    
-    def ensure_correct_user
-      @user = User.find(params[:id])
-      redirect_to root_path unless current_user?(@user)
-    end
-    
-    def ensure_admin_user
-      redirect_to users_path unless current_user.admin?
     end
 end
